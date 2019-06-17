3d52d2d106c0cb275f0fb352f61680a133ceafb7
lorenzojokhan <lorenzojokhan@hotmail.com>
2019-06-17 20:03:35 +0200

    Added animation to bird

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    [SerializeField]
+    private float upForce = 200f;
+    public bool isDead;
+
+    private Animator anim;
+    private Rigidbody2D body;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (!isDead)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                anim.SetTrigger("Flap");
+                body.velocity = Vector2.zero;
+                body.AddForce(Vector2.up * upForce);
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        anim.SetTrigger("Die");
+        isDead = true;
+    }
+}
